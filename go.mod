596511b01bcd0c9efb4256d452f9160100383f35
Pablo Antipan <antipan.pablo@gmail.com>
2023-03-26 11:11:38 -0300

    add server

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go/golang-api-rest
+
+go 1.20
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ server/server.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DatabaseUrl string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (broker *Broker) Config() *Config {
+	return broker.config
+}
+
+func NewServer(context context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("port is required")
+	}
+	if config.JWTSecret == "" {
+		return nil, errors.New("jwt secret is required")
+	}
+	if config.DatabaseUrl == "" {
+		return nil, errors.New("database url is required")
+	}
+
+	return &Broker{config: config, router: mux.NewRouter()}, nil
+}
+
+func (broker *Broker) Start(binder func(server Server, router mux.Router)) {
+	broker.router = mux.NewRouter()
+	binder(broker, *broker.router)
+	log.Println("Starting server on port", broker.Config().Port)
+	if err := http.ListenAndServe(broker.config.Port, broker.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
